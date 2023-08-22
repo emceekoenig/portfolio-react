5fdcbb25c80e899782d313a0b4135b159c9a426c
emceekoenig <129642888+emceekoenig@users.noreply.github.com>
2023-08-22 01:29:30 -0500

    Updated about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -18,8 +18,7 @@ const About = () => {
         <div className="max-w-[1000px] w-full grid sm:grid-cols-2 gap-8 px-4">
           <div className="sm:text-right text-4xl font-bold">
             <p>
-              Front End Developer with 8 years of experience working in the HR
-              Tech industry.
+              Front End Developer with 8 years of experience working in HR Tech.
             </p>
           </div>
           <div>
